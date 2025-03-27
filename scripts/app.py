4394c944a50e4b2a0cc0c61c2a5a77674c17728c
Walid <didoune.walid@gmail.com>
2025-03-27 16:17:23 +0100

    v1.3 - finalisation de l'api et préparation déploiement azure

--- scripts/app.py
+++ scripts/app.py
@@ -3,11 +3,29 @@ import joblib
 import shap
 import numpy as np 
 import pandas as pd
+import os
 
 
 app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-model = joblib.load("../models/LogisticRegression_model.joblib")
+model_path = os.path.join(BASE_DIR, "..", "models", "LogisticRegression_model.joblib")
+model = joblib.load(model_path)
+
+df_path = os.path.join(BASE_DIR, "..", "data", "cleaned", "df.csv")
+df = pd.read_csv(df_path, sep=";", encoding="utf-8")
+
+clf = model.named_steps["clf"]
+feature_names = model.feature_names
+coefficients = clf.coef_.flatten()
+coeff_df = pd.DataFrame({
+  "feature": feature_names,
+  "coefficients": coefficients,
+  "abs_coefficients": np.abs(coefficients)
+})
+coeff_df = coeff_df.sort_values(by="abs_coefficients", ascending=False)
+coeff_df_top = coeff_df.head(10)[["feature", "coefficients"]].to_dict(orient="records")
+
 
 @app.route("/")
 def home():
@@ -18,5 +36,22 @@ def home():
 def predict():
   # Récupération des données envoyées en JSON
   data = request.get_json()
-
+  sk_id = data["SK_ID_CURR"]
   
+  sample = df[df["SK_ID_CURR"] == sk_id].copy()
+  if sample.empty:
+    return jsonify({"erreur": "SK_ID_CURR introuvable."}), 404
+  
+  sample.drop(columns=["SK_ID_CURR", "TARGET"], inplace=True)
+  
+  prediction = model.predict(sample)
+  prediction_proba = model.predict_proba(sample)[:, 1]
+  
+  return jsonify({
+    "prediction" : prediction.tolist(),
+    "proba_classe_1" : float(prediction_proba[0]),
+    "top_20_coefficients" : coeff_df_top
+    })
+
+if __name__ == "__main__":
+  app.run(debug=True, port=5000)
